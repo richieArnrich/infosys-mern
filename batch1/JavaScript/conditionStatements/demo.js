fe065ab4001e9b14f06078ef0acf51831ad25e51
richieArnold <arnoldrichie31.rar@gmail.com>
2024-11-22 20:00:13 +0530

    add switch case

---
+++ batch1/JavaScript/conditionStatements/demo.js
@@ -0,0 +1,16 @@
+let value = 100;
+switch (value) {
+  case value < 200:
+    {
+      console.log("value is less than 200");
+    }
+    break;
+  case value > 100:
+    {
+      console.log("value is greater than 100");
+    }
+    break;
+  default: {
+    console.log("idiot, in case values i cannot check for conditions");
+  }
+}
