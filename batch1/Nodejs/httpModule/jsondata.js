57843f98baece86dd493b3983b32ecc87283dc44
richieArnold <arnoldrichie31.rar@gmail.com>
2025-01-13 18:24:50 +0530

    add json display and fetch

--- batch1/Nodejs/httpModule/jsondata.js
+++ batch1/Nodejs/httpModule/jsondata.js
@@ -1,21 +1,27 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader({ "Access-Control-Allow-Origin": "*" });
-  res.setHeader({ "Access-Control-Allow-Methods": "GET" });
-  res.setHeader({
-    "content-type": "application/json",
-  });
+  // Set appropriate headers for CORS and content type
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Content-Type", "application/json");
+
+  // Set response status code
   res.writeHead(200);
+
+  // Define the JSON response
   const message = {
     message: "Hello, World!",
-    name: "John Doe",
-    age: 23,
-    proffession: "Doctor",
+    Name: "John Doe",
+    Age: 23,
+    Profession: "Doctor",
   };
+
+  // Send the response
   res.end(JSON.stringify(message));
 });
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
+// Start the server
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
