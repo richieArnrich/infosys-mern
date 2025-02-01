da4829a9c7c3b98fda7eb73873065f92f3debd43
richieArnold <arnoldrichie31.rar@gmail.com>
2025-02-01 17:27:44 +0530

    add crud-app

---
+++ batch1/crud-app/client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-body-tertiary text-center text-lg-start">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
+        © 2020 Copyright:
+        <a className="text-body" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
